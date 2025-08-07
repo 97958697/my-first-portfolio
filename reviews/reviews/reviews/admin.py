112db5238a7ef004c0c0c0f3d081161295906a5d
S0002368631 <daikisegawa@icloud.com>
2025-08-07 12:08:44 +0900

    Restore flattened accounts and reviews

--- reviews/reviews/reviews/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Movie, Review
-
-admin.site.register(User)
-admin.site.register(Movie)
-admin.site.register(Review)
-# Register your models here.
