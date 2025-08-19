484b6e7510b6fab4d484e1bda0c0fb0a2bc5ac45
S0002368631 <daikisegawa@icloud.com>
2025-08-19 09:52:34 +0900

    Allow GET logout redirect to login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import LogoutGetView
 
 app_name = 'accounts'
 
@@ -8,6 +9,6 @@ urlpatterns = [
     # ログイン／ログアウト／サインアップ
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     # ログアウト後はログインページへリダイレクト
-    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
+    path('logout/', LogoutGetView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+
+class LogoutGetView(LogoutView):
+    """Allow logout via GET and POST, redirect to login page."""
+    http_method_names = ['get', 'post']
+    next_page = reverse_lazy('accounts:login')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 def signup(request):
     if request.method == 'POST':
