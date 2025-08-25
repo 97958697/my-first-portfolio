9cb644867eb12f32ac6233cc394f5f416c044d6a
S0002368631 <daikisegawa@icloud.com>
2025-08-25 17:57:53 +0900

    PythonAnywhere 用に TEMPLATES DIRS を修正

--- second_project/settings.py
+++ second_project/settings.py
@@ -58,10 +58,7 @@ ROOT_URLCONF = 'second_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'portfolio' / '01_URLDispatch' / 'first_project' / 'templates',
-            BASE_DIR / 'templates',
-        ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
