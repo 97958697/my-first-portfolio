8ea6a6551781621909c71bde6e9695fb3b26870f
S0002368631 <daikisegawa@icloud.com>
2025-08-26 10:15:17 +0900

    Fix STATICFILES_DIRS path for PythonAnywhere

--- second_project/settings.py
+++ second_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    # Root static (portfolio/static)
+    # PythonAnywhere root static (~/portfolio/static)
     BASE_DIR.parent.parent / 'static',
     # App static (first_project/static)
     BASE_DIR / 'static',
